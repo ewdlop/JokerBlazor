50af77525ab6bc9f1b8e27a4496b9770fe111260
daydin14 <david.aydin6@gmail.com>
2024-08-16 21:51:51 -0400

    Serilog for Server-Side

---
+++ JokerBlazor/JokerBlazor/JokerBlazor/Settings/SerilogSettings.cs
@@ -0,0 +1,33 @@
+﻿using Serilog;
+
+namespace JokerBlazor.Settings
+{
+    /// <summary>
+    /// Represents the settings for configuring Serilog.
+    /// </summary>
+    public class SerilogSettings
+    {
+        /// <summary>
+        /// Configures Serilog using the settings from the appsettings.json file.
+        /// </summary>
+        public void ConfigureSerilog()
+        {
+            try
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+                Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(configuration)
+                    .CreateLogger();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while configuring Serilog in appSettings.json : {ex}");
+                throw;
+            }
+        }
+    }
+}
